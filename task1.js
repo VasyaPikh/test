ec826db32016b16ee5a3f63a2a335372a1177f16
VasyaPikh <pikhvasya@gmail.com>
2022-01-29 11:02:17 +0000

    Task 1 rework

--- task1.js
+++ task1.js
@@ -1,9 +1,22 @@
-const arr = [6, 7, 88, 5, 35, 75];
-const res = arr.reduce((a, c)=>{
-  if(c % 5 == 0 && c % 7 !== 0){
-    a.sum += c;
-    a.number++
+// const arr = [6, 7, 88, 5, 35, 75];
+// const res = arr.reduce((a, c)=>{
+//  if(c % 5 == 0 && c % 7 !== 0){
+//    a.sum += c;
+//    a.number++
+//  }
+//  return a
+// }, {sum:0, number:0});
+// console.log("Кількість: %i, сума: %i", res.sum, res.number)
+
+
+const n = 100;
+ 
+let sum = 0;
+let total = 0;
+for (let i = 0; i <= n; i += 5) {
+  if (i % 7 !== 0) {
+    sum += i;
+    total++;
   }
-  return a
-}, {sum:0, number:0});
-console.log("Кількість: %i, сума: %i", res.sum, res.number)
+}
+console.log(total, sum);
