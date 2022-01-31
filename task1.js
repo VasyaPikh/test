6d749e284babd3139ff27980f625df442ee9a128
VasyaPikh <pikhvasya@gmail.com>
2022-01-31 12:11:28 +0200

    Initial commit

--- task1.js
+++ task1.js
@@ -1,14 +1,3 @@
-// const arr = [6, 7, 88, 5, 35, 75];
-// const res = arr.reduce((a, c)=>{
-//  if(c % 5 == 0 && c % 7 !== 0){
-//    a.sum += c;
-//    a.number++
-//  }
-//  return a
-// }, {sum:0, number:0});
-// console.log("Кількість: %i, сума: %i", res.sum, res.number)
-
-
 const n = 100;
  
 let sum = 0;
